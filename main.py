cf97c6290e87ee6728986992b98666ec65ded536
SinghSahitya <sahityasingh1211@gmail.com>
2023-09-20 23:06:20 +0530

    final

--- main.py
+++ main.py
@@ -15,10 +15,19 @@ quiz = Quiz(question_list)
 
 print("#########################################################")
 print("\nWelcome to MyQuiz\n")
+print("Instructions:\n1. Answer either 'True' or 'False'.\n2. You can skip the questions by typing 'skip'.\n3. Skipped questions can be attempted at the end of the quiz.")
+print("")
 
 while quiz.question_remaining():
     quiz.next_question()
 
+if quiz.skipped_ques:
+    user_choice = input("\nYou have skipped questions. Would you like to attempt them? (yes/no): ")
+    if user_choice.lower() == 'yes':
+        quiz.answer_skipped_questions()
+
 print("#########################################################\n")
 print(f"Your Score: {quiz.score} out of {2*len(quiz.ques_list)}")
 print("Thank You!")
+
+
--- quiz_model.py
+++ quiz_model.py
@@ -3,22 +3,35 @@ class Quiz:
         self.score = 0
         self.ques_num = 0
         self.ques_list = ques_list
+        self.skipped_ques = []
 
     def next_question(self):
         current_question = self.ques_list[self.ques_num]
         self.ques_num += 1
-        print(f"Q.{self.ques_num}: {current_question.text}? ")
+        print(f"\nQ.{self.ques_num}: {current_question.text}? ")
         user_answer = input('Your answer: ')
-        self.check_answer(user_answer, current_question.answer)
+
+        if user_answer.lower() == 'skip':
+            print("Question Skipped!")
+            self.skipped_ques.append(current_question)
+        else: 
+            self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, u_ans, c_ans):
         if u_ans.lower() == c_ans.lower():
             self.score += 2
-            print("Correct!!")
+            print("Correct!")
         else:
             self.score -= 1
-            print("Wrong!!")
+            print("Wrong!")
         print()  
 
     def question_remaining(self):
         return self.ques_num < len(self.ques_list)
+
+    def answer_skipped_questions(self):
+        for skipped_question in self.skipped_ques:
+            print(f"Skipped Question: {skipped_question.text}")
+            user_input = input("Your answer: ")
+            self.check_answer(user_input, skipped_question.answer)
+        self.skipped_ques = []
